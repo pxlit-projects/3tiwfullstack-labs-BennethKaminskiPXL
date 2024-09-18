021955b3b75014379376543276895968a8f34af8
BennethKaminskiPXL <118831756+BennethKaminskiPXL@users.noreply.github.com>
2024-09-18 19:49:22 +0200

    department-service structure stuck List<Employee>

---
+++ backend-java/SpringCloudDemo/department-service/src/main/java/be/pxl/services/domain/Employee.java
@@ -0,0 +1,20 @@
+package be.pxl.services.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+}
