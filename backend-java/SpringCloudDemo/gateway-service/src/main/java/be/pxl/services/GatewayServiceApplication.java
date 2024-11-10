7e514ada716df4fe87c79f170aa8c0242fa3a660
BennethKaminskiPXL <118831756+BennethKaminskiPXL@users.noreply.github.com>
2024-11-10 16:45:59 +0100

    lab 7 before removing spring-boot-starter-web from pom

---
+++ backend-java/SpringCloudDemo/gateway-service/src/main/java/be/pxl/services/GatewayServiceApplication.java
@@ -0,0 +1,20 @@
+package be.pxl.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * GatewayServiceApplication
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServiceApplication
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
